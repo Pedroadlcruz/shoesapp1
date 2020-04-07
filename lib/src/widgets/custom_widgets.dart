298921b37de3a400906b7911004086eb74b5a4eb
pedroadlcruz <pedroadlcruz@gmail.com>
2020-04-07 01:01:13 -0400

    UI First Page ready

--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,6 +1,13 @@
 
 export  'package:shoesapp1/src/widgets/custom_appbar.dart';
 export  'package:shoesapp1/src/widgets/zapato_size.dart';
+export  'package:shoesapp1/src/widgets/zapato_desc.dart';
+export  'package:shoesapp1/src/widgets/agregar_carrito.dart';
+export  'package:shoesapp1/src/widgets/boton_naranja.dart';
+
+
+
+
 
 
 
